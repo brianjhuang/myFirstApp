88e7c3c3f8e3d7eefdee00bea369f22e030d8394
brianjhuang <brianjhuang25@gmail.com>
2021-03-06 22:33:32 -0800

    Initial Commit

---
+++ myFirstApp/myFirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  myFirstAppApp.swift
+//  myFirstApp
+//
+//  Created by Brian Huang on 3/6/21.
+//
+
+import SwiftUI
+
+@main
+struct myFirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
